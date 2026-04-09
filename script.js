f10036ae63c865b7699f21c9c67157ad6ab9b927
TabadaLotis <tistabada@gmail.com>
2026-04-09 23:45:17 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,15 +1,27 @@
+// Define helloSpeaker object
+var helloSpeaker = {};
+helloSpeaker.speak = function(name) {
+    console.log("Hello " + name);
+    document.getElementById("greetings").innerHTML += "Hello " + name + "<br>";
+};
+
+// Define goodbyeSpeaker object
+var goodbyeSpeaker = {};
+goodbyeSpeaker.speak = function(name) {
+    console.log("Goodbye " + name);
+    document.getElementById("greetings").innerHTML += "Goodbye " + name + "<br>";
+};
+
 // Array of names
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
-// Loop over each name
+// Loop through names and print greetings
 for (var i = 0; i < names.length; i++) {
     var firstLetter = names[i].charAt(0).toLowerCase();
 
     if (firstLetter === 'j') {
-        // If name starts with 'j' or 'J', use goodbyeSpeaker
         goodbyeSpeaker.speak(names[i]);
     } else {
-        // Otherwise, use helloSpeaker
         helloSpeaker.speak(names[i]);
     }
 }
